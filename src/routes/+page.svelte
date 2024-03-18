a39b999cd84c8d14f5d95b76fc131abda79186b1
firebat75 <jacklowry1337@gmail.com>
2024-03-18 12:49:32 -0400

    added alchemy sdk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
+<script>
+    import { PUBLIC_API_KEY } from "$env/static/public";
+    import { Alchemy, Network } from "alchemy-sdk";
+    import { onMount } from "svelte";
+
+    const settings = {
+        apiKey: PUBLIC_API_KEY,
+        network: Network.ETH_MAINNET,
+    };
+
+    const alchemy = new Alchemy(settings);
+
+    /**
+     * @type {number}
+     */
+    let block;
+    async function getBlock() {
+        block = await alchemy.core.getBlockNumber();
+    }
+
+    onMount(async () => {
+        getBlock();
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<p>Current Block: {block}</p>
